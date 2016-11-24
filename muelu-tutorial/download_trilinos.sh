cca3881490355ad7c05b1f3d07c241f6e01a84a0
Tobias Wiesner <tawiesn@sandia.gov>
2016-11-24 14:36:35 -0700

    muelu-tutorial: files for MueLu tutorial image
    The image contains a Trilinos 12.8.1 installation with the MueLu tutorial.
    It installs a VNC server that can be used to connect to the docker container
    for using the GUI in the tutorial
    
    The vnc password is stored in password.txt

---
+++ muelu-tutorial/download_trilinos.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://trilinos.org/oldsite/download/files/trilinos-12.8.1-Source.tar.gz
+#wget https://github.com/trilinos/Trilinos/archive/develop.zip
+
---
+++ muelu-tutorial/install_trilinos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -i
+
+# create folder structure and move files to correct place
+TRILINOS_SRCDIR=~/trilinos
+TRILINOS_BUILDDIR=~/trilinos-build
+
+TPL_BASEDIR=/opt/tpls
+
+## load MPI module
+echo "Load MPI module"
+module load mpi/openmpi-x86_64
+
+## run configuration
+cd ~
+./do-configure
+
+## build trilinos
+cd $TRILINOS_BUILDDIR
+make -j 4
+cd ~
+
+## copy MueLu tutorial data
+ln -s ~/trilinos-build/packages/muelu/doc/Tutorial/src muelu-tutorial
